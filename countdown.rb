cb908fd09df136fbd7b6c15976f1340875c471e4
Srikalyan Megati <kalmegati@gmail.com>
2019-09-11 19:47:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,12 @@
 #write your code here
 
-def countdown
+def countdown(tMinus)
   
+  while tMinus > 0
+    puts "#{tMinus} SECOND(S)!"
+    tMinus -= 1 
+  end
   
+  puts "HAPPY NEW YEAR!"
   
 end
